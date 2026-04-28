a1a1bdc7a8d701336274c42e6a9ce5c63afff560
eugeniolarrain <eugeniolarraincastro@gmail.com>
2026-04-28 18:58:48 -0400

    Cambios en pagina

--- gestion-autos/app/page.tsx
+++ gestion-autos/app/page.tsx
@@ -1,4 +1,27 @@
+
+"use client"
+
+import { signIn, signOut, useSession } from "next-auth/react"
+
 export default function Home() {
+  const { data: session } = useSession()
+
+  if (!session) {
+    return (
+      <button onClick={() => signIn("google")}>
+        Login con Google
+      </button>
+    )
+  }
+
+  return (
+    <>
+      <p>Hola {session.user?.name}</p>
+      <button onClick={() => signOut()}>
+        Cerrar sesión
+      </button>
+    </>
+  )
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-slate-900 dark:to-slate-800">
       <h1 className="text-6xl font-bold text-slate-900 dark:text-white">
